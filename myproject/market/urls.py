34362429853131479482c819e64d91f1a4dcf0c5
Juk-Juk <ybanezcesar24@gmail.com>
2025-10-03 21:44:25 -0300

    added cart

--- myproject/market/urls.py
+++ myproject/market/urls.py
@@ -1,4 +1,4 @@
-from market.views import product_create, product_list, product_delete, my_product_list
+from market.views import product_create, product_list, product_delete, my_product_list, view_cart, add_to_cart, remove_from_cart
 from django.urls import path
 
 app_name = "market"
@@ -8,4 +8,7 @@ urlpatterns = [
     path('my_products', my_product_list, name="my_product_list"),
     path("create/", product_create, name="product_create"),
     path('products/<int:pk>/delete/', product_delete, name='product_delete'),
+    path("cart/", view_cart, name="view_cart"),
+    path("add/<int:product_id>/", add_to_cart, name="add_to_cart"),
+    path('remove/<int:product_id>/', remove_from_cart, name='remove_from_cart'),
 ]
--- myproject/market/views.py
+++ myproject/market/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .forms import ProductForm
-from .models import Product
+from .models import Product, Cart, CartItem
 from django.contrib import messages
 
 @login_required
@@ -45,7 +45,7 @@ def product_edit(request, pk):
             return redirect("market:product_list")
     else:
         form = ProductForm(instance=product)
-    return render(request, "market/product_form.html", {"form": form})
+    return render(request, "product_form.html", {"form": form})
 
 #Delete Product
 @login_required
@@ -54,5 +54,29 @@ def product_delete(request, pk):
     if request.method == "POST":
         product.active = False
         product.save()
-        return redirect("market:product_list")
-    return render(request, "market/product_list.html", {"product": product})
+        return redirect("market:my_product_list")
+    return render(request, "my_product_list.html", {"product": product})
+
+@login_required
+def add_to_cart(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    cart, created = Cart.objects.get_or_create(user=request.user)
+    item, created = CartItem.objects.get_or_create(cart=cart, product=product)
+    item.quantity += 1
+    item.save()
+    messages.success(request, 'Producto agregado al carrito')
+    return redirect("market:view_cart")
+
+@login_required
+def remove_from_cart(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    cart = get_object_or_404(Cart, user=request.user)
+    cart_item = get_object_or_404(CartItem, cart=cart, product=product)
+    cart_item.delete()
+    messages.success(request, 'Producto eliminado del carrito')
+    return redirect("market:view_cart")
+
+@login_required
+def view_cart(request):
+    cart, created = Cart.objects.get_or_create(user=request.user)
+    return render(request, "shopping_cart.html", {"cart": cart})
