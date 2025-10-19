ef7cc3f815d4d3f8be3a25f8bda4be827b178bd7
Juk-Juk <ybanezcesar24@gmail.com>
2025-10-18 21:21:10 -0300

    small adjustments

--- myproject/core/views.py
+++ myproject/core/views.py
@@ -26,15 +26,15 @@ def home(request):
           password1 = request.POST.get('password1')
           password2 = request.POST.get('password2')
           
-          if password1 == password2:
+          if password1 != password2:
+              messages.error(request, 'Las contraseñas no coinciden')
+          else:
               try:
                   user = User.objects.create_user(username=username, email=email, password=password1)
-                  messages.success(request, '¡Cuenta creada exitosamente!')
-                  login(request, user)
-              except:
+              except Exception as e:
                   messages.error(request, 'Error al crear la cuenta')
-          else:
-              messages.error(request, 'Las contraseñas no coinciden')
+              else:
+                  messages.success(request, '¡Cuenta creada exitosamente! ')
           return redirect('/')
       elif form_type == 'pwreset':
           form = PasswordResetForm(request.POST)
@@ -51,30 +51,30 @@ def home(request):
           return redirect('/')
     return render(request, 'home.html')
 
-# SignUp View
-def signup_view(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, '¡Cuenta creada exitosamente! Inicia sesión.')
-            return redirect('/')
-    else:
-        form = UserCreationForm()
-    return render(request, 'account/signup.html', {'form': form})
+# # SignUp View
+# def signup_view(request):
+#     if request.method == 'POST':
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request, '¡Cuenta creada exitosamente! Inicia sesión.')
+#             return redirect('/')
+#     else:
+#         form = UserCreationForm()
+#     return render(request, 'account/signup.html', {'form': form})
 
-# Login View
-def login_view(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            messages.success(request, f"Bienvenido, {user.username}!")
-            return redirect('home')
-    else:
-        form = AuthenticationForm()
-    return render(request, 'account/login.html', {'form': form})
+# # Login View
+# def login_view(request):
+#     if request.method == 'POST':
+#         form = AuthenticationForm(data=request.POST)
+#         if form.is_valid():
+#             user = form.get_user()
+#             login(request, user)
+#             messages.success(request, f"Bienvenido, {user.username}!")
+#             return redirect('home')
+#     else:
+#         form = AuthenticationForm()
+#     return render(request, 'account/login.html', {'form': form})
 
 # Logout View
 def logout_view(request):
@@ -82,6 +82,6 @@ def logout_view(request):
     messages.success(request, "La sesión se ha cerrado correctamente.")
     return redirect('/')
 
-# Password Reset View
-def password_reset_view(request):
-    return PasswordResetView.as_view(template_name='account/password_reset.html')(request)
+# # Password Reset View
+# def password_reset_view(request):
+#     return PasswordResetView.as_view(template_name='account/password_reset.html')(request)
