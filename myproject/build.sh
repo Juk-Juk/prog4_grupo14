9e65e19ad1e7e60feaadec6133325b7a413bbff4
Juk <Jukzhee@gmail.com>
2025-11-17 21:43:08 -0300

    build fix

--- myproject/build.sh
+++ myproject/build.sh
@@ -11,4 +11,13 @@ python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
 # createsuperuser
-python manage.py createsuperuser --no-input
+echo "Checking if a superuser exists"
+python manage.py shell <<EOF
+from django.contrib.auth.models import User
+if not User.objects.filter(is_superuser=True).exists():
+    print("Creating superuser")
+    from django.core.management import call_command
+    call_command('createsuperuser', '--no-input')
+else:
+    print("Superuser already exists. Skipping creation.")
+EOF
